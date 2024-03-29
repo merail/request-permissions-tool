60bd2b6ab58e70b32022643fee902308fa66c75f
merail <r.mesherov@blanc.ru>
2024-03-29 22:49:24 +0300

    Remove unnecessary files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,6 @@ android {
             minCompileSdk = 21
         }
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
         namespace = "me.rail.tools.permissions"
     }
 
@@ -49,8 +47,4 @@ afterEvaluate {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
