50f0ec94a0a8ecb757ca79785efe5beee0a82efd
merail <r.mesherov@blanc.ru>
2024-04-11 21:21:39 +0300

    Update compose lib in dev

--- dev/build.gradle.kts
+++ dev/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling")
     implementation("androidx.compose.ui:ui-tooling-preview")
